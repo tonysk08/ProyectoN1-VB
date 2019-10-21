79e6bb3d9234ce18bf6fec507838fd8a1e8ffa3e
maitcsan23 <macuervo84@gmail.com>
2019-10-20 21:18:47 -0500

    Cambio en pantalla de asientos disponibles

--- Proyecto1DS8/RevisarAsientos.Designer.vb
+++ Proyecto1DS8/RevisarAsientos.Designer.vb
@@ -28,6 +28,18 @@ Partial Class RevisarAsientos
         Me.ConsultarPelículasYTandasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.VerAsientosDisponiblesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.VerTotalesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblAsientos1 = New System.Windows.Forms.Label()
+        Me.lblAsientos2 = New System.Windows.Forms.Label()
+        Me.lblAsientos3 = New System.Windows.Forms.Label()
+        Me.lblAsientos4 = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -38,8 +50,8 @@ Partial Class RevisarAsientos
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BienvenidaToolStripMenuItem, Me.IngresarPelículaYTandasToolStripMenuItem, Me.ConsultarPelículasYTandasToolStripMenuItem, Me.VerAsientosDisponiblesToolStripMenuItem, Me.VerTotalesToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(5, 2, 0, 2)
-        Me.MenuStrip1.Size = New System.Drawing.Size(1148, 36)
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(861, 29)
         Me.MenuStrip1.TabIndex = 3
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -47,44 +59,175 @@ Partial Class RevisarAsientos
         '
         Me.BienvenidaToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!)
         Me.BienvenidaToolStripMenuItem.Name = "BienvenidaToolStripMenuItem"
-        Me.BienvenidaToolStripMenuItem.Size = New System.Drawing.Size(121, 32)
+        Me.BienvenidaToolStripMenuItem.Size = New System.Drawing.Size(98, 25)
         Me.BienvenidaToolStripMenuItem.Text = "Bienvenida"
         '
         'IngresarPelículaYTandasToolStripMenuItem
         '
         Me.IngresarPelículaYTandasToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!)
         Me.IngresarPelículaYTandasToolStripMenuItem.Name = "IngresarPelículaYTandasToolStripMenuItem"
-        Me.IngresarPelículaYTandasToolStripMenuItem.Size = New System.Drawing.Size(254, 32)
+        Me.IngresarPelículaYTandasToolStripMenuItem.Size = New System.Drawing.Size(205, 25)
         Me.IngresarPelículaYTandasToolStripMenuItem.Text = "Ingresar películas y tandas"
         '
         'ConsultarPelículasYTandasToolStripMenuItem
         '
         Me.ConsultarPelículasYTandasToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!)
         Me.ConsultarPelículasYTandasToolStripMenuItem.Name = "ConsultarPelículasYTandasToolStripMenuItem"
-        Me.ConsultarPelículasYTandasToolStripMenuItem.Size = New System.Drawing.Size(267, 32)
+        Me.ConsultarPelículasYTandasToolStripMenuItem.Size = New System.Drawing.Size(215, 25)
         Me.ConsultarPelículasYTandasToolStripMenuItem.Text = "Consultar películas y tandas"
         '
         'VerAsientosDisponiblesToolStripMenuItem
         '
         Me.VerAsientosDisponiblesToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!)
         Me.VerAsientosDisponiblesToolStripMenuItem.Name = "VerAsientosDisponiblesToolStripMenuItem"
-        Me.VerAsientosDisponiblesToolStripMenuItem.Size = New System.Drawing.Size(235, 32)
+        Me.VerAsientosDisponiblesToolStripMenuItem.Size = New System.Drawing.Size(189, 25)
         Me.VerAsientosDisponiblesToolStripMenuItem.Text = "Ver asientos disponibles"
         '
         'VerTotalesToolStripMenuItem
         '
         Me.VerTotalesToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.VerTotalesToolStripMenuItem.Name = "VerTotalesToolStripMenuItem"
-        Me.VerTotalesToolStripMenuItem.Size = New System.Drawing.Size(119, 32)
+        Me.VerTotalesToolStripMenuItem.Size = New System.Drawing.Size(96, 25)
         Me.VerTotalesToolStripMenuItem.Text = "Ver Totales"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(95, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 39)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Sala 1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(95, 178)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 39)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Sala 2:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(95, 274)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 39)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Sala 3:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(95, 362)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(130, 39)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Sala 4:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(312, 80)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(349, 39)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Asientos disponibles"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(312, 178)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(349, 39)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Asientos disponibles"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(312, 274)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(349, 39)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Asientos disponibles"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(312, 362)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(349, 39)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Asientos disponibles"
+        '
+        'lblAsientos1
+        '
+        Me.lblAsientos1.AutoSize = True
+        Me.lblAsientos1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAsientos1.Location = New System.Drawing.Point(247, 80)
+        Me.lblAsientos1.Name = "lblAsientos1"
+        Me.lblAsientos1.Size = New System.Drawing.Size(37, 39)
+        Me.lblAsientos1.TabIndex = 12
+        Me.lblAsientos1.Text = "0"
+        '
+        'lblAsientos2
+        '
+        Me.lblAsientos2.AutoSize = True
+        Me.lblAsientos2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAsientos2.Location = New System.Drawing.Point(247, 178)
+        Me.lblAsientos2.Name = "lblAsientos2"
+        Me.lblAsientos2.Size = New System.Drawing.Size(37, 39)
+        Me.lblAsientos2.TabIndex = 13
+        Me.lblAsientos2.Text = "0"
+        '
+        'lblAsientos3
+        '
+        Me.lblAsientos3.AutoSize = True
+        Me.lblAsientos3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAsientos3.Location = New System.Drawing.Point(247, 274)
+        Me.lblAsientos3.Name = "lblAsientos3"
+        Me.lblAsientos3.Size = New System.Drawing.Size(37, 39)
+        Me.lblAsientos3.TabIndex = 14
+        Me.lblAsientos3.Text = "0"
+        '
+        'lblAsientos4
+        '
+        Me.lblAsientos4.AutoSize = True
+        Me.lblAsientos4.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAsientos4.Location = New System.Drawing.Point(247, 362)
+        Me.lblAsientos4.Name = "lblAsientos4"
+        Me.lblAsientos4.Size = New System.Drawing.Size(37, 39)
+        Me.lblAsientos4.TabIndex = 15
+        Me.lblAsientos4.Text = "0"
         '
         'RevisarAsientos
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1148, 554)
+        Me.ClientSize = New System.Drawing.Size(861, 450)
+        Me.Controls.Add(Me.lblAsientos4)
+        Me.Controls.Add(Me.lblAsientos3)
+        Me.Controls.Add(Me.lblAsientos2)
+        Me.Controls.Add(Me.lblAsientos1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.MenuStrip1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "RevisarAsientos"
         Me.Text = "RevisarAsientos"
         Me.MenuStrip1.ResumeLayout(False)
@@ -100,4 +243,16 @@ Partial Class RevisarAsientos
     Friend WithEvents ConsultarPelículasYTandasToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents VerAsientosDisponiblesToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents VerTotalesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblAsientos1 As Label
+    Friend WithEvents lblAsientos2 As Label
+    Friend WithEvents lblAsientos3 As Label
+    Friend WithEvents lblAsientos4 As Label
 End Class
